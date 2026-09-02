6075e5222cb12ee7fea99f4c71a5fa620ea9a492
kutwusu <kutwusu0929@gmail.com>
2026-09-03 00:07:54 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE='lumis-shell-v1';
+const SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL))));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{
+  const u=new URL(e.request.url);
+  if(u.pathname.endsWith('/data/deck.json')){
+    e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request)));
+    return;
+  }
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
