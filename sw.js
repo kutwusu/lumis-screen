9c55fbf3db1e44868ce51b0ac6cc625bdae299b1
kutwusu <kutwusu0929@gmail.com>
2026-09-03 01:02:51 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='lumis-shell-v1';
+const CACHE='lumis-shell-v2';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL))));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
